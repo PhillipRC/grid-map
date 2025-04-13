9eb35e5a58323236489e4281acf6d72763572616
PhillipRC <phillip.r.cargo@gmail.com>
2025-04-13 07:19:56 -0400

    update to add wasd and angle directions

--- app/src/app-main/app-main.ts
+++ app/src/app-main/app-main.ts
@@ -47,15 +47,6 @@ export default class AppMain extends GridBase {
       )
     }
 
-    // c key
-    if (event.key == 'c') {
-      this.dispatchEvent(
-        new CustomEvent(
-          'grid-map-display-clear', { bubbles: true, detail: null }
-        )
-      )
-    }
-
     // grid-map-data-generate-random
     if (event.key == 'g') {
       this.dispatchEvent(
@@ -65,8 +56,13 @@ export default class AppMain extends GridBase {
       )
     }
 
-    // arrow keys
-    if (event.keyCode >= 37 && event.keyCode <= 40) {
+    // arrow keys - move horizontal and vertical
+    if (
+      event.key == 'ArrowRight'
+      || event.key == 'ArrowLeft'
+      || event.key == 'ArrowUp'
+      || event.key == 'ArrowDown'
+    ) {
 
       this.dispatchEvent(
         new CustomEvent(
@@ -81,6 +77,51 @@ export default class AppMain extends GridBase {
         )
       )
     }
+
+    // wasd - move horizontal and vertical
+    if (
+      event.key == 'd'
+      || event.key == 'a'
+      || event.key == 'w'
+      || event.key == 's'
+    ) {
+
+      this.dispatchEvent(
+        new CustomEvent(
+          'grid-map-cursor-move',
+          {
+            bubbles: true,
+            detail: {
+              x: (event.key == 'd' ? 1 : event.key == 'a' ? -1 : 0),
+              y: (event.key == 'w' ? -1 : event.key == 's' ? 1 : 0),
+            },
+          }
+        )
+      )
+    }
+
+    // qezc - move at an angle
+    if (
+      event.key == 'q'
+      || event.key == 'e'
+      || event.key == 'z'
+      || event.key == 'c'
+    ) {
+
+      this.dispatchEvent(
+        new CustomEvent(
+          'grid-map-cursor-move',
+          {
+            bubbles: true,
+            detail: {
+              x: ((event.key == 'q' || event.key == 'z') ? -1 : (event.key == 'e' || event.key == 'c') ? 1 : 0),
+              y: ((event.key == 'q' || event.key == 'e') ? -1 : (event.key == 'z' || event.key == 'c') ? 1 : 0),
+            },
+          }
+        )
+      )
+    }
+
 
   }
 
